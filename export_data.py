a9e70e96e5b9ec90dc41996ff03ef3cdb4408861
Long Xin Jie <95921560+longxinjie@users.noreply.github.com>
2025-09-18 00:45:33 +0800

    update export_data.py

--- export_data.py
+++ export_data.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def export_data():
-
-    cred = credentials.Certificate("../smu-fintech-mvp-2025-firebase-adminsdk-fbsvc-f9c8e62f9b.json")
+    
+    cred_path = os.getenv("FIREBASE_CREDENTIALS")
+    cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred)
     db = firestore.client()
     all_txns = []
@@ -17,7 +21,7 @@ def export_data():
                 all_txns.append(d)
 
     df = pd.DataFrame(all_txns)
-    df.to_csv("data/app_transactions.csv", index=False)  # 👈 here it’s created
+    df.to_csv("data/app_transactions.csv", index=False)  
     print(f"Exported {len(df)} rows to data/app_transactions.csv")
 
 if __name__ == "__main__":
